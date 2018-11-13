db850adbdd3145c2d8e6b97e79c3b38467d79a43
Naveed Hotak <P16224424@LEC-ADMIN.dmu.ac.uk>
2018-11-13 17:57:04 +0000

    added the layout of the website

---
+++ javascript.js
@@ -0,0 +1,13 @@
+const hello = document.getElementById("Hello");
+
+	console.log.(hello);
+	// hello.textContent = "Goodbye";
+	
+	
+	hello.addEventListener("click", greetings);
+	
+	
+	funcation greeting() {
+	 alert ("Hello!");
+	 hello.textContent = "Goodbye";
+	{
